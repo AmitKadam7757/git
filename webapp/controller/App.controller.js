e58de8fea4a25d34a7205668b4e34ea7afae67db
Amit Kadam <amit.a.kadam@capgemini.com>
2024-03-14 09:44:18 +0000

    update_1

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("forgit.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"for_git/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
